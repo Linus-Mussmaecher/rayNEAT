fe09b08000efdf10131fa85f09043a835431996d
Linus-Mussmaecher <linus.mussmaecher@gmail.com>
2022-04-06 23:51:48 +0200

    Updated XOR scenario and CMakeLists so project may build again after previous commit

--- code/main.cpp
+++ code/main.cpp
@@ -3,7 +3,6 @@
 
 //scenarios
 #include "scenarios/XOR/XOR.h"
-#include "scenarios/snake/snake.h"
 
 int main() {
     SetRandomSeed(clock());
--- code/scenarios/XOR/XOR.cpp
+++ code/scenarios/XOR/XOR.cpp
@@ -6,21 +6,16 @@
 
 void testXOR(){
 
-    NeatInstance neatInstance = NeatInstance();
-    neatInstance.input_count = 3;
-    neatInstance.output_count = 1;
+    NeatInstance neatInstance = NeatInstance(3,1,100);
     neatInstance.generation_target = 100;
-    neatInstance.population = 100;
     neatInstance.folderpath = "./XOR";
     neatInstance.runNeat(&testNetworkXOR);
 
-    Network best = Network::loadNthBest("./XOR/NEAT_Generation_100.rn");
+    Network best = neatInstance.getNetworksSorted()[0];
 
     for (int a = 0; a < 2; a++) {
         for (int b = 0; b < 2; b++) {
-            best.setInputs({float(a), float(b), 1.f});
-            best.calculateOutputs();
-            std::cout << a << " ^ " << b << " = " << best.getOutputs()[0] << "\n";
+            std::cout << a << " ^ " << b << " = " << best.calculate({float(a), float(b), 1.f})[0] << "\n";
         }
     }
 }
@@ -29,9 +24,7 @@ int testNetworkXOR(Network n){
     float res = 0;
     for (int a = 0; a < 2; a++) {
         for (int b = 0; b < 2; b++) {
-            n.setInputs({float(a), float(b), 1.f});
-            n.calculateOutputs();
-            res += abs(n.getOutputs()[0] - float(a ^ b));
+            res += abs(n.calculate({float(a), float(b), 1.f})[0] - float(a ^ b));
         }
     }
     return int((4.f - res) * (4.f - res) * 100);
